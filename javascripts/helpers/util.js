cd637b0f0e72d971a1466219f49644186228c550
Silvestre Luna <slvtrluna@gmail.com>
2019-04-12 23:26:17 -0500

    wip

--- javascripts/helpers/util.js
+++ javascripts/helpers/util.js
@@ -8,8 +8,11 @@ const printToDom = (divId, textToPrint) =>{
 const selectionEvents = (e) => {
     e.preventDefault();
     const currentId = e.target.id;
+    const bread = breads.getBread();
+    let domString = `<p>${currentId}</p>`;
+     domString += `<p>${bread[currentId]}</p>`;
 
-    
+    printToDom('printSandwich', domString);
 };
 
 const eventL = () => {
