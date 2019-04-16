eb507ff000313374f262ffa381f0fd1aba35764a
Silvestre Luna <slvtrluna@gmail.com>
2019-04-15 19:25:54 -0500

    completed styling to total

--- javascripts/helpers/util.js
+++ javascripts/helpers/util.js
@@ -19,16 +19,8 @@ const allVeggies = veggie.getVeggies();
 const allItems = Object.assign(allMeat, allCondiments, cheeses, allVeggies, allBread);
 
 
-
-
-// let selectedBread = [];
-// let selectedMeat = [];
-// let selectedCondiment = [];
-// let selectedCheese = [];
-// let selectedVeggies = [];
 let checkoutSelection = [];
 
-let totalPrices = [];
 const selectionEvents = (e) => {
     // e.preventDefault();
     const currentId = e.target.id;
@@ -52,119 +44,6 @@ const selectionEvents = (e) => {
 };
 
 
-const getTotal = () => {
-    let tp = [];
-    let top = 0 ;
-    checkoutSelection.forEach((p) => {
-        top += allItems[p];
-        console.log(top);
-    });
-    printToDom('totalP', top);
-};
-
-
-// const selectionEvents = (e) => {
-//    // e.preventDefault();
-//    const currentId = e.target.id;
-//    const currentItem = document.getElementById(currentId);
-//    if(currentItem.checked) {
-//        if(selectedBread.includes(currentId)) {
-
-//        } else {
-//            selectedBread.push(currentId);
-
-//        }
-//    } else if (!currentItem.checked) {
-//        if(selectedBread.includes(currentId)) {
-//            document.getElementById('printSandwich').innerHTML = '';
-//         selectedBread = selectedBread.filter(bread => bread !== currentId);
-//    };
-//     } 
-//     domBuilder(selectedBread);
-//     console.log(selectedBread);
-// };
-
-
-// const selectionEvents2 = (e) => {
-//     // e.preventDefault();
-//     const currentId = e.target.id;
-//     const currentItem = document.getElementById(currentId);
-//     if(currentItem.checked) {
-//         if(selectedMeat.includes(currentId)) {
-
-//         } else {
-//             selectedMeat.push(currentId);
-
-//         }
-//     } else if (!currentItem.checked) {
-//         if(selectedMeat.includes(currentId)) {
-//            document.getElementById('printMeat').innerHTML = '';
-//             selectedMeat = selectedMeat.filter(meat => meat !== currentId);
-//     } 
-//      };
-//      domBuilder2(selectedMeat);
-//  };
-
-//  const selectionEvents3 = (e) => {
-//     // e.preventDefault();
-//     const currentId = e.target.id;
-//     const currentItem = document.getElementById(currentId);
-//     if(currentItem.checked) {
-//         if(selectedCondiment.includes(currentId)) {
-
-//         } else {
-//             selectedCondiment.push(currentId);
-
-//         }
-//     } else if (!currentItem.checked) {
-//         if(selectedCondiment.includes(currentId)) {
-//            document.getElementById('printCondiment').innerHTML = '';
-//            selectedCondiment = selectedCondiment.filter(c => c !== currentId);
-//     } 
-//      };
-//      domBuilder3(selectedCondiment);
-//  };
-// //events Buttons
-// const selectionEvents4 = (e) => {
-//     // e.preventDefault();
-//     const currentId = e.target.id;
-//     const currentItem = document.getElementById(currentId);
-//     if(currentItem.checked) {
-//         if(selectedCheese.includes(currentId)) {
-
-//         } else {
-//             selectedCheese.push(currentId);
-
-//         }
-//     } else if (!currentItem.checked) {
-//         if(selectedCheese.includes(currentId)) {
-//            document.getElementById('printCheese').innerHTML = '';
-//            selectedCheese = selectedCheese.filter(chz => chz !== currentId);
-//     } 
-//      };
-//      domBuilder4(selectedCheese);
-//  };
-
-//  const selectionEvents5 = (e) => {
-//     // e.preventDefault();
-//     const currentId = e.target.id;
-//     const currentItem = document.getElementById(currentId);
-//     if(currentItem.checked) {
-//         if(selectedVeggies.includes(currentId)) {
-
-//         } else {
-//             selectedVeggies.push(currentId);
-
-//         }
-//     } else if (!currentItem.checked) {
-//         if(selectedVeggies.includes(currentId)) {
-//            document.getElementById('printVeggies').innerHTML = '';
-//            selectedVeggies = selectedVeggies.filter(veg => veg !== currentId);
-//     } 
-//      };
-//      domBuilder5(selectedVeggies);
-//  };
-
 
 const eventL = (e) => {
     //const targetId = e.target.id
@@ -172,98 +51,22 @@ const eventL = (e) => {
 
     for (let i = 0; i < currentButton.length; i++) {
         currentButton[i].addEventListener('click', selectionEvents);
-    }
-    document.getElementById('finishOrder').addEventListener('click', getTotal);
-
-
-    //    document.getElementById('white').addEventListener('click', selectionEvents);
-    //    document.getElementById('wheat').addEventListener('click', selectionEvents);
-    //    document.getElementById('whole').addEventListener('click', selectionEvents);
-
-    //    document.getElementById('ham').addEventListener('click', selectionEvents2);
-    //    document.getElementById('chicken').addEventListener('click', selectionEvents2);
-    //    document.getElementById('tuna').addEventListener('click', selectionEvents2);
-
-    //    document.getElementById('ranch').addEventListener('click', selectionEvents3);
-    //    document.getElementById('mayonnaise').addEventListener('click', selectionEvents3);
-    //    document.getElementById('mustard').addEventListener('click', selectionEvents3);
-
-    //    document.getElementById('provolone').addEventListener('click', selectionEvents4);
-    //    document.getElementById('swiss').addEventListener('click', selectionEvents4);
-    //    document.getElementById('american').addEventListener('click', selectionEvents4);
-
-    //    document.getElementById('lettuce').addEventListener('click', selectionEvents5);
-    //    document.getElementById('tomatoes').addEventListener('click', selectionEvents5);
-    //    document.getElementById('spinach').addEventListener('click', selectionEvents5);
+    };
 };
 
 
 
 const domBuilder = (array) => {
     let domString = '';
+    let top = 0;
     array.forEach((item) => {
         domString += `<p>${item} $${allItems[item]} </p>`;
+        top += allItems[item];
         printToDom('printSandwich', domString);
-
+        printToDom('total', `<h4>Total: $${top}</h4>`);
     });
+
 };
-
-
-
-// const domBuilder = (array) => {
-
-//     let domString = '';
-//     array.forEach((bread) => {
-//     domString += `<p>${bread} bread $${allBread[bread]} </p>`
-
-//     printToDom('printSandwich', domString);
-// });
-
-// }
-
-// const domBuilder2 = (array) => {
-
-//     let domString = '';
-//     array.forEach((meat) => {
-//     domString += `<p>${meat} $${allMeat[meat]} </p>`
-
-//     printToDom('printMeat', domString);
-// });
-
-// }
-
-// const domBuilder3 = (array) => {
-
-//     let domString = '';
-//     array.forEach((c) => {
-//     domString += `<p>${c} $${allCondiments[c]} </p>`
-
-//     printToDom('printCondiment', domString);
-// });
-
-// }
-
-// const domBuilder4 = (array) => {
-
-//     let domString = '';
-//     array.forEach((chz) => {
-//     domString += `<p>${chz} cheese $${cheeses[chz]} </p>`
-
-//     printToDom('printCheese', domString);
-// });
-
-// }
-
-// const domBuilder5 = (array) => {
-
-//     let domString = '';
-//     array.forEach((veg) => {
-//     domString += `<p>${veg} $${allVeggies[veg]} </p>`
-
-//     printToDom('printVeggies', domString);
-// });
-
-// }
 
 
 export default { eventL };
